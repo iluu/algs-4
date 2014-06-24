217e326666661a29d08795d850d39ed3f52890eb
Karolina Rusin <karolina.rusin@gmail.com>
2014-06-24 12:01:31 +0200

    First commit - project setup

---
+++ src/Percolation.java
@@ -0,0 +1,41 @@
+public class Percolation {
+
+    /**
+     * Create N by B grid, with all sites blocked
+     */
+    public Percolation(int N) {
+
+    }
+
+    /**
+     * Open site at i, j if it is not already open
+     *
+     * @param i row
+     * @param j column
+     */
+    public void open(int i, int j) {
+
+    }
+
+    /**
+     * @return true if site at i, j is open, false otherwise
+     */
+    public boolean isOpen(int i, int j) {
+        return false;
+    }
+
+    /**
+     * @return true if site at i, j is full, false otherwise
+     */
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+
+    /**
+     * @return true if system percolates, false otherwise
+     */
+    public boolean percolates() {
+        return false;
+    }
+
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,46 @@
+public class PercolationStats {
+
+    /**
+     * Perform T independent computational experiments on an N-by-N grid
+     * @param N dimensions of the array
+     * @param T number of experiments to run
+     */
+    public PercolationStats(int N, int T){
+
+    }
+
+    /**
+     * @return sample mean of percolation threshold
+     */
+    public double mean(){
+        return 0.0;
+    }
+
+    /**
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev(){
+        return 0.0;
+    }
+
+    /**
+     * @return lower bound of the 95% confidence interval
+     */
+    public double confidenceLo(){
+        return 0.0;
+    }
+
+    /**
+     * @return upper bound of the 95% confidence interval
+     */
+    public double confidenceHi(){
+        return 0.0;
+    }
+
+    /**
+     * Test client
+     */
+    public static void main(String[] args){
+
+    }
+}
